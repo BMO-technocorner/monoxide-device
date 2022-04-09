bd683ce843bfc9a58d1cf275583a71cae6513ab1
Ezra Lazuardy <ezralucio@gmail.com>
2022-04-09 13:55:25 +0700

    fix: failing ci build

--- lib/env.py
+++ lib/env.py
@@ -1,15 +1,22 @@
 from os.path import isfile
 
 Import("env")
-assert isfile(".env")
+
+env_file = isfile(".env")
+
+if not env_file:
+    print("File .env not found!")
+
+assert env_file
+
 try:
-  f = open(".env", "r")
-  lines = f.readlines()
-  envs = []
-  for line in lines:
-    envs.append("-D {}".format(line.strip()))
-  env.Append(BUILD_FLAGS=envs)
+    f = open(".env", "r")
+    lines = f.readlines()
+    envs = []
+    for line in lines:
+        envs.append("-D {}".format(line.strip()))
+    env.Append(BUILD_FLAGS=envs)
 except IOError:
-  print("File .env not accessible",)
+    print("File .env not accessible")
 finally:
-  f.close()
+    f.close()
